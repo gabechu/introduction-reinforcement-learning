7b2ddb316fc4282bd192c4f9082300aca5378118
gchu <gchu@zendesk.com>
2023-06-24 11:53:31 +1000

    added calc for exploit

--- src/chapter_2/ten_armed_bandits/action_tracker.py
+++ src/chapter_2/ten_armed_bandits/action_tracker.py
@@ -1,6 +1,8 @@
 from typing import List
 
 import numpy as np
+import numpy.typing as npt
+from matplotlib.axes import Axes
 
 
 class ActionTracker(object):
@@ -30,25 +32,38 @@ class ActionTrackerPool(object):
     """A pool of action trackers."""
 
     def __init__(self, num_bandits: int, num_actions) -> None:
-        self._trackers = [ActionTracker(num_actions) for _ in range(num_bandits)]
+        self._num_bandits = num_bandits
+        self.trackers = [ActionTracker(num_actions) for _ in range(num_bandits)]
 
     def get_action_tracker(self, bandit: int) -> ActionTracker:
         """Fetch action tracker for the specific bandit problem."""
-        return self._trackers[bandit]
+        return self.trackers[bandit]
 
-    def calculate_percentage_of_optimal_action(self, optimal_action: int):
+    def _coalesce_actions(self) -> npt.NDArray:
+        """Combine actions from different bandits into a matrix with the shape (num_bandits, num_steps)."""
+        return np.array([tracker.actions for tracker in self.trackers])
+
+    def calculate_percentage_of_optimal_action(self, optimal_action: int) -> npt.NDArray:
         """Calculate the percentage of optimal action taken per step."""
-        all_actions = np.array([tracker.actions for tracker in self._trackers])
+        all_actions = self._coalesce_actions()
+        return (all_actions == optimal_action).sum(axis=0) / self._num_bandits
 
-        percentage_by_step = []
-        num_steps = all_actions.shape[1]
-        for i in range(num_steps):
-            subset_actions = all_actions[:, i]
-            percentage = (subset_actions == optimal_action).sum() / subset_actions.size
-            percentage_by_step.append(percentage)
-        return percentage_by_step
-
-    def plot_percentage_of_optimal_action(self, ax, optimal_action: int, label: str):
-        """Plot mean rewards across different steps."""
+    def plot_percentage_of_optimal_action(self, ax: Axes, optimal_action: int, label: str):
+        """Plot mean rewards across steps."""
         ax.plot(self.calculate_percentage_of_optimal_action(optimal_action), label=label)
+        ax.legend()
+
+    def calculate_percentage_of_exploit(self) -> npt.NDArray:
+        """Calculate the percentage of exploit behaviors per step. The exploit behavior is identified when the
+        current action is the same as the previous action."""
+        all_actions = self._coalesce_actions()
+        num_steps = all_actions.shape[1]
+        step_n_actions = all_actions[:, : num_steps - 1]
+        step_n_plus_one_actions = all_actions[:, 1:]
+
+        return (step_n_actions == step_n_plus_one_actions).sum(axis=0) / self._num_bandits
+
+    def plot_percentage_of_exploit(self, ax: Axes, label: str):
+        """Plot the percentage of exploit behaviors across steps."""
+        ax.plot(self.calculate_percentage_of_exploit(), label=label)
         ax.legend()
