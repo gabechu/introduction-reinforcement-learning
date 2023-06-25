5b8d83933895a19d22ebdde12c8e412afd106748
gchu <gchu@zendesk.com>
2023-06-25 20:24:08 +1000

    optimise possion reward gen

--- src/chapter_2/ten_armed_bandits/action_reward_simulator.py
+++ src/chapter_2/ten_armed_bandits/action_reward_simulator.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from functools import cached_property
+from typing import Dict, List
 
 import numpy as np
 import numpy.typing as npt
@@ -38,7 +39,7 @@ class NormalActionRewardSimulator(ActionRewardSimulator):
     """Simulating action rewards from normal distributions."""
 
     @cached_property
-    def _cache(self):
+    def _cache(self) -> Dict[int, List]:
         return {
             action: list(self._stateful_generator.normal(reward, 1, size=50_000))
             for action, reward in enumerate(self._true_action_rewards)
@@ -47,7 +48,7 @@ class NormalActionRewardSimulator(ActionRewardSimulator):
     def _set_true_rewards(self) -> npt.NDArray:
         return self._stateful_generator.standard_normal(size=self._num_actions)
 
-    def generate_reward(self, action: int):
+    def generate_reward(self, action: int) -> float:
         if len(self._cache.get(action)) == 0:
             del self.__dict__["_cache"]
         return self._cache.get(action).pop()
@@ -56,17 +57,27 @@ class NormalActionRewardSimulator(ActionRewardSimulator):
 class PoissonActionRewardSimulator(ActionRewardSimulator):
     """Simulating action rewards from Poisson distributions."""
 
+    @cached_property
+    def _cache(self):
+        return {
+            action: list(self._stateful_generator.poisson(reward, size=50_000))
+            for action, reward in enumerate(self._true_action_rewards)
+        }
+
     def _set_true_rewards(self) -> npt.NDArray:
-        print("Execute once")
         return self._stateful_generator.poisson(3, size=self._num_actions)
 
     def generate_reward(self, action: int) -> float:
-        return self._stateful_generator.poisson(self._true_action_rewards[action], size=1).item()
+        if len(self._cache.get(action)) == 0:
+            del self.__dict__["_cache"]
+        return self._cache.get(action).pop()
 
 
 class RandomWalkRewardSimulator(ActionRewardSimulator):
     """Simulating action rewards from normal distributions, where each action reward samples from
-    a distribution with a mean value that follows a random walk."""
+    a distribution with a mean value that follows a random walk.
+    Note: running this simulator coudl be slow as it has not been optimised yet.
+    """
 
     def _set_true_rewards(self) -> npt.NDArray:
         return np.zeros(self._num_actions)
