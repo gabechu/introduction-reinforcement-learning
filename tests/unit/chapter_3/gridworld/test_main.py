71bfb87f5e2828d11aea2c15824f7463c6b9f1bb
Gabe <wei.g.chu@gmail.com>
2023-08-13 09:46:18 +1000

    rename run to main

--- tests/unit/chapter_3/gridworld/test_main.py
+++ tests/unit/chapter_3/gridworld/test_main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pytest import fixture
 
-from src.chapter_3.gridworld.main import calculate_new_state_value
+from src.chapter_3.gridworld.run import calculate_new_state_value
 from src.chapter_3.gridworld.state import State
 
 
