ce27a0ee2f687db25deeb8d765f605d6dbd05453
Gabe <wei.g.chu@gmail.com>
2023-08-19 11:06:03 +1000

    solving gridworld for optimal policy

--- src/chapter_3/gridworld/run.py
+++ src/chapter_3/gridworld/run.py
@@ -14,30 +14,41 @@ logger = logging.getLogger(__file__)
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-def calculate_state_value_function(
+def calculate_state_value_function_for_random_policy(
     state: State,
     state_value_matrix: np.ndarray,
-    discount_factor: float = 0.8,
+    discount_factor: float,
 ) -> float:
-    # evaluate the policy where the agent randomly selects an action
-    actions = [Action.NORTH, Action.SOUTH, Action.EAST, Action.WEST]
-    action_probability = 1 / len(actions)
+    action_probability = 1 / len(Action)
 
     v_value = 0
-    for action in actions:
+    for action in Action:
         next_state = regularize_state(get_next_state(action=action, current_state=state))
         next_reward = evaluate_reward(current_state=state, action=action)
-        # Calcualte q_\pi(s, a)
-        # The probabilities of both p(r | s, a, s') and p(s' | s, a). This is because, given the
-        # current state and an action, the outcomes for both reward and the next state are deterministic
+        # The probabilities of both p(r | s, a, s') and p(s' | s, a) are one. So they are omitted. The reason that
+        # those probabilities are zero is that given the current state and an action, the outcomes for both reward
+        # and the next state are deterministic
         q_value = next_reward + discount_factor * get_state_value(next_state, state_value_matrix)
-        # update v_\pi(s)
         v_value += action_probability * q_value
 
     return v_value
 
 
-if __name__ == "__main__":
+def calculate_state_value_function_for_optimal_polic(
+    state: State, state_value_matrix: np.ndarray, discount_factor: float
+) -> float:
+    q_values = []
+
+    for action in Action:
+        next_state = regularize_state(get_next_state(action=action, current_state=state))
+        next_reward = evaluate_reward(current_state=state, action=action)
+        q_value = next_reward + discount_factor * get_state_value(next_state, state_value_matrix)
+        q_values.append(q_value)
+
+    return max(q_values)
+
+
+def run_calculation_for_random_policy():
     # a 5 x 5 grid
     state_value_matrix = np.zeros(shape=(5, 5))
     states = [State(x=x, y=y) for x in range(5) for y in range(5)]
@@ -47,7 +58,7 @@ if __name__ == "__main__":
         # sweep states and update
         for state in states:
             old_state_value = get_state_value(state, state_value_matrix)
-            new_state_value = calculate_state_value_function(state, state_value_matrix, 0.9)
+            new_state_value = calculate_state_value_function_for_random_policy(state, state_value_matrix, 0.9)
 
             update_state_value_matrix(
                 state=state,
@@ -57,9 +68,42 @@ if __name__ == "__main__":
 
             diff += np.absolute(new_state_value - old_state_value)
 
-        logging.debug("Before and after the update difference %s", diff)
+        logger.debug("Before and after the update difference %s", diff)
 
         if np.isclose(diff, 0):
-            logging.info("Converged in %s steps...", i + 1)
-            logging.info(state_value_matrix)
+            logger.info("Converged in %s steps...", i + 1)
+            logger.info(state_value_matrix)
             break
+
+
+def run_calculation_for_optimal_policy():
+    # a 5 x 5 grid
+    state_value_matrix = np.zeros(shape=(5, 5))
+    states = [State(x=x, y=y) for x in range(5) for y in range(5)]
+
+    for i in range(1000):
+        diff = 0
+        # sweep states and update
+        for state in states:
+            old_state_value = get_state_value(state, state_value_matrix)
+            new_state_value = calculate_state_value_function_for_optimal_polic(state, state_value_matrix, 0.9)
+
+            update_state_value_matrix(
+                state=state,
+                new_state_value=new_state_value,
+                state_value_matrix=state_value_matrix,
+            )
+
+            diff += np.absolute(new_state_value - old_state_value)
+
+        logger.debug("Before and after the update difference %s", diff)
+
+        if np.isclose(diff, 0):
+            logger.info("Converged in %s steps...", i + 1)
+            logger.info(state_value_matrix)
+            break
+
+
+if __name__ == "__main__":
+    # run_calculation_for_random_policy()
+    run_calculation_for_optimal_policy()
--- tests/unit/chapter_3/gridworld/test_run.py
+++ tests/unit/chapter_3/gridworld/test_run.py
@@ -1,7 +1,9 @@
+from unittest import TestCase
+
 import numpy as np
 from pytest import fixture
 
-from src.chapter_3.gridworld.run import calculate_state_value_function
+from src.chapter_3.gridworld.run import calculate_state_value_function_for_random_policy
 from src.chapter_3.gridworld.state import State
 
 
@@ -18,26 +20,34 @@ def state_value_matrix() -> np.ndarray:
     )
 
 
-def test_calculate_state_value_function_for_state_A(state_value_matrix):
-    actual = calculate_state_value_function(State(0, 1), state_value_matrix, 0.9)
-    assert actual == 8.83
+class TestStateValueFunctionForRandomPolicy(TestCase):
+    def setUp(self) -> None:
+        self.state_value_matrix = np.array(
+            [
+                [3.3, 8.8, 4.4, 5.3, 1.5],
+                [1.5, 3.0, 2.3, 1.9, 0.5],
+                [0.1, 0.7, 0.7, 0.4, -0.4],
+                [-1.0, 0.4, -0.4, -0.6, -1.2],
+                [-1.9, -1.3, -1.2, -1.4, -2.0],
+            ]
+        )
 
+    def test_state_A(self):
+        actual = calculate_state_value_function_for_random_policy(State(0, 1), self.state_value_matrix, 0.9)
+        assert actual == 8.83
 
-def test_calculate_state_value_function_for_state_B(state_value_matrix):
-    actual = calculate_state_value_function(State(0, 3), state_value_matrix, 0.9)
-    assert actual == 5.36
+    def test_state_B(self):
+        actual = calculate_state_value_function_for_random_policy(State(0, 3), self.state_value_matrix, 0.9)
+        assert actual == 5.36
 
+    def test_state_00(self):
+        actual = calculate_state_value_function_for_random_policy(State(0, 0), self.state_value_matrix, 0.9)
+        assert actual == 3.3025
 
-def test_calculate_state_value_function_for_state_00(state_value_matrix):
-    actual = calculate_state_value_function(State(0, 0), state_value_matrix, 0.9)
-    assert actual == 3.3025
+    def test_state_04(self):
+        actual = calculate_state_value_function_for_random_policy(State(0, 4), self.state_value_matrix, 0.9)
+        assert actual == 1.48
 
-
-def test_calculate_state_value_function_for_state_04(state_value_matrix):
-    actual = calculate_state_value_function(State(0, 4), state_value_matrix, 0.9)
-    assert actual == 1.48
-
-
-def test_calculate_state_value_function_for_state_11(state_value_matrix):
-    actual = calculate_state_value_function(State(1, 1), state_value_matrix, 0.9)
-    assert actual == 2.9925
+    def test_state_11(self):
+        actual = calculate_state_value_function_for_random_policy(State(1, 1), self.state_value_matrix, 0.9)
+        assert actual == 2.9925
